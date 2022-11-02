1e212636444a47101ba50d6ea0995347a7397cdf
damajojung <jungdamajo@gmail.com>
2022-11-02 10:39:22 +0100

    Saving

--- freeCodeCamp/Streamlit/playground_app.py
+++ freeCodeCamp/Streamlit/playground_app.py
@@ -17,5 +17,14 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+### Close Line Chart
+""")
+
 st.line_chart(tickerDf.Close)
+
+st.write("""
+### Volume Line Chart
+""")
+
 st.line_chart(tickerDf.Volume)
